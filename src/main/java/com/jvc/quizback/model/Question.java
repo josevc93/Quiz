2b544862b79278f12510afadcb242d6056247a44
José Manuel Vázquez Crespo <josemanuel.vazquezcrespo@gmail.com>
2018-05-16 17:52:14 +0200

    Add relation between question and tag

--- src/main/java/com/jvc/quizback/model/Question.java
+++ src/main/java/com/jvc/quizback/model/Question.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -17,6 +19,8 @@ import lombok.Setter;
 @Setter
 @Entity
 public class Question {
+	public static final String FIELD_TAG = "tag";
+	
 	@Id
 	@GeneratedValue
 	private Integer idQuestion;
@@ -28,4 +32,8 @@ public class Question {
 	@JoinTable (name="questionQuestionary", joinColumns= {@JoinColumn(name="idQuestion")},
 			   inverseJoinColumns= {@JoinColumn(name="idQuestionary")})
 	private List<Questionary> questionary;
+	
+	@JoinColumn(name = FIELD_TAG)
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Tag tag;
 }
